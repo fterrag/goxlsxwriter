aeec17141ae4679efac9534405af9d29ffc1ec60
Frank Terragna <fterragna@gmail.com>
2017-03-15 01:00:33 -0400

    Added test for SetFontColor()

--- format_test.go
+++ format_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+func TestSetFontColor(t *testing.T) {
+	expectedPath := "resources/xlsx/SetFontColor.xlsx"
+
+	workbook, generatedPath := MakeTestWorkbook()
+	defer os.Remove(generatedPath)
+
+	worksheet := NewWorksheet(workbook, "Sheet 1")
+
+	format := NewFormat(workbook)
+	format.SetFontColor(0x009900)
+
+	worksheet.WriteString(0, 0, "Hello", format)
+	worksheet.WriteString(1, 0, "World!", nil)
+
+	workbook.Close()
+
+	CompareXlsxFiles(t, expectedPath, generatedPath)
+}
+
 func TestSetBackgroundColor(t *testing.T) {
 	expectedPath := "resources/xlsx/SetBackgroundColor.xlsx"
 
